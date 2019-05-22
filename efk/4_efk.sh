20385d098acaf400ad1ee781590ed862ca562d6c
dinesh kumar <dotnext24@gmail.com>
2019-05-23 00:26:56 +0530

    kibana

--- efk/4_efk.sh
+++ efk/4_efk.sh
@@ -1,7 +1,9 @@
-helm install --name es-operator \
-    --namespace efk \
-    akomljen-charts/elasticsearch-operator
+kubectl apply -f kibana.yaml
 
-helm install --name efk \
-    --namespace efk \
-    akomljen-charts/efk
+# helm install --name es-operator \
+#     --namespace efk \
+#     akomljen-charts/elasticsearch-operator
+
+# helm install --name efk \
+#     --namespace efk \
+#     akomljen-charts/efk
