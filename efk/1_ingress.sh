c264ef19af51b3b3bc173f6f06836d247d74c6b0
dinesh kumar <dotnext24@gmail.com>
2019-05-22 17:35:47 +0530

    command

--- efk/1_ingress.sh
+++ efk/1_ingress.sh
@@ -1,7 +1,4 @@
 kubectl create namespace efk
 
 # Use Helm to deploy an NGINX ingress controller
-helm install stable/nginx-ingress \
-    --namespace efk \
-    --set controller.replicaCount=2 \
-    --set nodeSelector."beta.kubernetes.io/os"=linux
+helm install stable/nginx-ingress --namespace efk --set controller.replicaCount=2 --set nodeSelector."beta.kubernetes.io/os"=linux
