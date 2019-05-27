80f793ced556ae49c6acb463892cdd2578ae68d2
Dinesh Kumar <Dinesh.Kumar3@ey.com>
2019-05-27 14:32:37 +0530

    efk

--- efk/2_dns.sh
+++ efk/2_dns.sh
@@ -4,7 +4,7 @@
 IP="23.96.203.14"
 
 # Name to associate with public IP address
-DNSNAME="elasticsearch-recipesavants"
+DNSNAME="efk-recipesavants"
 
 # Get the resource-id of the public ip
 PUBLICIPID=$(az network public-ip list --query "[?ipAddress!=null]|[?contains(ipAddress, '$IP')].[id]" --output tsv)
